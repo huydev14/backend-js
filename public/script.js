6352203ffcbb5ad597a2d44342f7099504aa6154
trgiahuy14 <trgiahuy14@gmail.com>
2026-01-28 23:46:26 +0700

    My first DOM test

--- public/script.js
+++ public/script.js
@@ -1,19 +1,7 @@
-const fetchUser = async () => {
-	const res = await fetch("http://localhost:8000/users");
-	const data = await res.json();
-	console.log(data);
+const submitBtn = document.getElementById('submitBtn');
+const todoInput = document.getElementById('todo');
 
-	const tbody = document.querySelector("#users tbody");
-
-	if(data && data.length) { 
-		data.forEach((user, index) => {
-			tbody.innerHTML += ` 
-				<tr>
-					<td>${user.id}</td>
-					<td>${user.name}</td>
-					<td>${user.email}</td>
-				</tr>`;
-		})
-	}
-	
-};
+submitBtn.addEventListener('click', () => {
+	const todoValue = todoInput.value
+	console.log(todoValue);
+})
